d389ca5b45ceaa4131784d9a8e901e87900a2fbe
Kritartha Adhikari <kritarthadybala10@gmail.com>
2026-05-26 22:48:45 +0545

    actions imported logic left to be implemented

--- aizen.py
+++ aizen.py
@@ -13,6 +13,7 @@ class Aizen:
         self.idleCount=0
         self.action="idle" #idle, sec_idle, walk, attack, hit, death
         self.attackCount=0
+        self.hitCount=0
 
     def draw(self, win):
         self.hitbox= pygame.Rect(self.x+10, self.y-4, 74, 74)
@@ -60,6 +61,78 @@ class Aizen:
             if self.attackCount+1>=limit:
                 self.attackCount=0
                 self.action="idle"
+        elif self.action=="attack":
+            limit=len(st.AizenattackRight)*framesPerImg
+            if self.facing==1:
+                sprite=st.AizenattackRight[self.attackCount//framesPerImg]
+            else:
+                sprite=st.AizenattackLeft[self.attackCount//framesPerImg]
+            if self.attackCount+1>=limit:
+                self.attackCount=0
+                self.action="jump_attack"
+            else:
+                self.attackCount+=1
+        elif self.action=="jump_attack":
+            limit=len(st.AizenJumpAttackLeft)*framesPerImg
+            if self.facing==1:
+                sprite=st.AizenJumpAttackRight[self.attackCount//framesPerImg]
+            else:
+                sprite=st.AizenJumpAttackLeft[self.attackCount//framesPerImg]
+            if self.attackCount+1>=limit:
+                self.attackCount=0
+                self.action="combo_attack"
+        elif self.action=="combo_attack":
+            limit=len(st.AizensecondAttackLeft)*framesPerImg
+            if self.facing==1:
+                sprite=st.AizensecondAttackRight[self.attackCount//framesPerImg]
+            else:
+                sprite=st.AizensecondAttackLeft[self.attackCount//framesPerImg]
+            if self.attackCount+1>=limit:
+                self.attackCount=0
+                self.action="idle"
+            else:
+                self.attackCount+=1
+        elif self.action=="teleport":
+            limit=len(st.AizenTeleportLeft)*framesPerImg
+            if self.facing==1:
+                sprite=st.AizenTeleportRight[self.idleCount//framesPerImg]
+            else:
+                sprite=st.AizenTeleportLeft[self.idleCount//framesPerImg]
+            if self.idleCount+1>=limit:
+                self.idleCount=0
+                self.action="idle"
+            else:
+                self.idleCount+=1
+        elif self.action=="walk":
+            limit=len(st.AizenRunRight)*framesPerImg
+            if self.facing==1:
+                sprite=st.AizenRunRight[self.walkCount//framesPerImg]
+            else:
+                sprite=st.AizenRunLeft[self.walkCount//framesPerImg]
+            if self.walkCount+1>=limit:
+                self.walkCount=0
+            else:
+                self.walkCount+=1
+        elif self.action=="hit":
+            limit=len(st.AizenHitLeft)*framesPerImg
+            if self.facing==1:
+                sprite=st.AizenHitRight[self.hitCount//framesPerImg]
+            else:
+                sprite=st.AizenHitLeft[self.hitCount//framesPerImg]
+            if self.hitCount+1>=limit:
+                self.hitCount=0
+            else:
+                self.hitCount+=1
+        elif self.action=="dash":
+            limit=len(st.AizenDashLeft)*framesPerImg
+            if self.facing==1:
+                sprite=st.AizenDashRight[self.idleCount//framesPerImg]
+            else:
+                sprite=st.AizenDashLeft[self.idleCount//framesPerImg]
+            if self.idleCount+1>=limit:
+                self.idleCount=0
+            else:
+                self.idleCount+=1
         else:
             limit=len(st.AizenFinalIdleRight)*framesPerImg
             if self.facing==-1:
